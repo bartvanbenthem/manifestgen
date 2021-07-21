63df166fcc1218783ccfc368a689debbec003436
Bartvanbenthem <bartvanbenthem@hotmail.com>
2021-07-21 10:41:34 +0200

    generator functions

--- app/manifestgen.go
+++ app/manifestgen.go
@@ -1,17 +1,21 @@
 package app
 
 import (
-	"fmt"
+	"encoding/json"
 	"html/template"
 	"io/ioutil"
 	"log"
 	"os"
-	"strings"
 
 	"gopkg.in/yaml.v2"
 )
 
-type ManifestGenClient struct{}
+type ManifestGenClient struct {
+}
+
+type Generator interface {
+	GenerateManifest(pathValuesFile, pathTemplateFile, pathOutputFile string)
+}
 
 func (c *ManifestGenClient) ConvertJSON2Yaml(pathJSONFile string) {
 }
@@ -19,14 +23,51 @@ func (c *ManifestGenClient) ConvertJSON2Yaml(pathJSONFile string) {
 func (c *ManifestGenClient) ConvertYaml2JSON(pathYamlFile string) {
 }
 
-func (c *ManifestGenClient) GenerateJSONManifest(pathValuesFile, pathTemplateFile, pathOutputFolder string) {
+func (c *ManifestGenClient) GenerateJSONManifest(pathValuesFile, pathTemplateFile, pathOutputFile string) {
+	// open the values yaml file
+	jsonFile, err := os.Open(pathValuesFile)
+	if err != nil {
+		log.Printf("Error: %v\n", err)
+	}
+	defer jsonFile.Close()
+
+	// read the values file and create a byte slice output
+	byteValue, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		log.Printf("Error: %v\n", err)
+	}
+	// create a map to store the unmarshalled byte slice
+	var values map[string]interface{}
+
+	// unmarshal byte slice into the values map
+	err = json.Unmarshal(byteValue, &values)
+	if err != nil {
+		log.Printf("Error: %v\n", err)
+	}
+
+	tpl, err := template.ParseFiles(pathTemplateFile)
+	if err != nil {
+		log.Printf("Error: %v\n", err)
+	}
+
+	genFile, err := os.Create(pathOutputFile)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer genFile.Close()
+
+	// Execute template var injection and write to file
+	err = tpl.Execute(genFile, values)
+	if err != nil {
+		log.Printf("Error: %v\n", err)
+	}
 }
 
 // Function to generate the configurable manifest from the values and template files
 // pathValuesFile wants a string containing path to the values yaml file
 // pathTemplateFile wants a string containing path to the template yaml file
 // pathOutputFolder wants a string containing path to the output folder
-func (c *ManifestGenClient) GenerateYamlManifest(pathValuesFile, pathTemplateFile, pathOutputFolder string) {
+func (c *ManifestGenClient) GenerateYamlManifest(pathValuesFile, pathTemplateFile, pathOutputFile string) {
 	// open the values yaml file
 	yamlFile, err := os.Open(pathValuesFile)
 	if err != nil {
@@ -53,9 +94,7 @@ func (c *ManifestGenClient) GenerateYamlManifest(pathValuesFile, pathTemplateFil
 		log.Printf("Error: %v\n", err)
 	}
 
-	valueFile := strings.Split(pathValuesFile, "/")
-	fileName := strings.Split(fmt.Sprintf(valueFile[len(valueFile)-1]), ".")
-	genFile, err := os.Create(fmt.Sprintf("%v/%v.yaml", pathOutputFolder, fileName[0]))
+	genFile, err := os.Create(pathOutputFile)
 	if err != nil {
 		log.Fatalln(err)
 	}
--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/bartvanbenthem/manifestgen/app"
 )
 
 func main() {
-	fmt.Printf("manifestgen\n")
+	var a app.ManifestGenClient
+	a.GenerateYamlManifest("testing/values/team.yaml", "testing/templates/team.yaml", "testing/output/team.yaml")
+	a.GenerateJSONManifest("testing/values/team.json", "testing/templates/team.json", "testing/output/team.json")
+	fmt.Printf("Done\n")
 }
