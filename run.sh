8219840b844b0ae190d450f107be13053c78f2ef
bartvanbenthem <bartvanbenthem@hotmail.com>
2023-09-22 07:00:44 +0200

    upd run

--- run.sh
+++ run.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+
+# rustup target add x86_64-unknown-linux-musl
+
 cargo test
-cargo build
-cargo build --release
+#cargo build
+#cargo build --release
+cargo build --target x86_64-unknown-linux-musl --release
 
 # run example to stdout
-cat project/example-var.json | ./target/release/manifestgen --template project/targets.tmpl
-#cat project/example-var.yaml | ./target/release/manifestgen --template project/targets.tmpl
-#cat project/invalid-input | ./target/release/manifestgen --template project/targets.tmpl
+cat project/example-var.json | ./target/x86_64-unknown-linux-musl/release/manifestgen --template project/targets.tmpl
+cat project/example-var.yaml | ./target/x86_64-unknown-linux-musl/release/manifestgen --template project/targets.tmpl
+#cat project/invalid-input | ./target/x86_64-unknown-linux-musl/release/manifestgen --template project/targets.tmpl
